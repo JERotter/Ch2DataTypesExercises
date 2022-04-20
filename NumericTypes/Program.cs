518a413ea916eba3d10641e89589bba41f07534c
JERotter <49504679+JERotter@users.noreply.github.com>
2022-04-20 16:58:34 -0400

    second commit with new project

---
+++ NumericTypes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace NumericTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter length of rectangle ");
+            double length = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter width of rectangle ");
+            double width = double.Parse(Console.ReadLine());
+            double area = length * width;
+            Console.WriteLine($"Area of the rectangle is {area} ");
+            Console.ReadLine();
+        }
+    }
+}
